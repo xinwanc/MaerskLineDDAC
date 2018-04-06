1db4965f3feec53711419b16c9afad49f9457811
xinwanc <38121289+xinwanc@users.noreply.github.com>
2018-04-06 11:32:29 +0800

    change code

--- PHPCode/ViewSchedule.php
+++ PHPCode/ViewSchedule.php
@@ -5,7 +5,7 @@ $limit = ( isset( $_GET['limit'] ) ) ? $_GET['limit'] : 5;
 $page = ( isset( $_GET['page'] ) ) ? $_GET['page'] : 1; //starting page
 $links = ( isset( $_GET['page'] ) ) ? $_GET['page'] : 7; 
 
-$query = "Select* from vesselschedule where CreatedBy='$currentAdminID' ORDER BY ID";
+$query = "Select* from vesselschedule where Status='A' and CreatedBy='$currentAdminID' ORDER BY ID";
 $paginator = new Paginator($loginconnect, $query);
 $results = $paginator->getData($limit, $page);
 
